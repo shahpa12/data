e0c51ca536d9f98d7df8499c21a8ca04a44d1509
shahpa12 <31307586+shahpa12@users.noreply.github.com>
2021-01-11 02:13:26 +0530

    let's try

---
+++ start.py
@@ -0,0 +1,35 @@
+# # if 5>10:
+# #  print("Hello let's begin")
+# # a=5
+# # b=2
+# # print("Sum",(a*b))
+# # print('''Hello how do you do is this capable for doing any lines of codes or what i am trying mine writing skills practice so that i can improve much more if required as soon as possible; is there any doubt''')
+# # # Boolean value testing
+# # d=input("Enter the no:")
+# # e=input("Enter second no:")
+# # d=int(d)
+# # e=int(e)
+# # c=d>e
+# # print(c)
+# # print("Type of d",type(d))
+# # print(type(e))
+# # # pranay=shahisthebest
+# # print(pranay)
+# shah="pranayshah"
+# print(shah[0::3])
+# a=int(input("Enter the no to check it is prime or not:"))
+# if a>1:
+#     for b in range(2,a):
+#         if(a%b)==0:
+#             print("It is not a prime no:")
+#             break
+#         else:
+#             print("It is a prime no:")
+#             break
+# else:
+#     print("Not a prime no:")
+
+
+for x in "Pranayshah":
+    print(x)
+
